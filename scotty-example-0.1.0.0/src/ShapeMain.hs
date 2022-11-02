f0046a97aad87b7cc5780f35462d6a964793171c
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2022-11-02 12:46:34 +0000

    Reinitialised

---
+++ scotty-example-0.1.0.0/src/ShapeMain.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Shapes
+import Render (render,defaultWindow)
+
+exampleDrawing =  [ (scale (point 0.5 0.25) <+> translate (point 1.2 0.4), mandelbrot) ]
+
+main = render "output.png" defaultWindow exampleDrawing
