25e69d035c221d597f63250e4839168861d320a3
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2022-11-08 15:21:03 +0000

    procrastination

--- Mandelbrot2/Mandelbrot2/src/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Shapes
-import Render (render,defaultWindow)
-
-exampleDrawing =  [ (scale (point 0.5 0.25) <+> translate (point 1.2 0.4), circle) ]
-exampleDrawing2 = [ (identity, mandelbrotset)]
-
-main = render "output.png" defaultWindow exampleDrawing2
