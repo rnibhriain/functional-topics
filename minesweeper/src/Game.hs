478f243d0bc7b8a6acf8f82ed573d7aa6fece6b3
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2022-12-19 16:11:32 +0000

    One Move Works - if you lose

--- minesweeper/src/Game.hs
+++ minesweeper/src/Game.hs
@@ -1,27 +1,60 @@
 module Game where
 
     import Data.List.Split
+    import Data.Char
     import Board
+
+    -- either 'f' or 'e' and a position
+    data Command = Command Char (Int, Int)
 
     game :: Board -> IO()
     game board = do
-        prettyPrintBoard board
-        putStrLn "Next Move"
-        --if True board
-          --  then
-            --    putStrLn "You Win!!"
-            --else do
-              --  putStrLn "Next Move"
-                --command <- getLine
-                --let move = resolveMove (commandToMove command) board
-                --if endsGame move board
-                --    then
-                --        gameOver board
-                --    else game (makeMove move board)
+        printBoard board
+        putStrLn "\nNext Move"
+        if checkWin board
+            then
+                putStrLn "Win!!\n"
+            else do
+                putStrLn "Enter your next Move\n"
+                command <- getLine
+                let move = convertCommand command
+                if isGameFinished move board
+                    then
+                        endGame board
+                    else putStrLn "Move entered\n"
+                    --game (makeMove move board)
 
-    prettyPrintBoard :: Board -> IO()
-    prettyPrintBoard (Board (x, _) grid _) = mapM_ putStrLn (splitEvery x (map printingReplacements grid))
+    isGameFinished :: Command -> Board -> Bool
+    isGameFinished (Command 'e' pos) board = checkForMine board (Command 'e' pos) 
+    isGameFinished (Command _ _) _ = False
+
+    convertCommand :: String -> Command
+    convertCommand cmd = Command move (x, y)
+        where 
+            move = head cmd
+            pos = drop 2 cmd
+            x = digitToInt (pos !! 0)
+            y = digitToInt (pos !! 2)
+
+    endGame:: Board -> IO()
+    endGame (Board (x, _) grid _) = mapM_ putStrLn (splitEvery x (map printingReplacements grid))
+
+    checkForMine :: Board -> Command -> Bool
+    checkForMine (Board _ cells _ ) (Command _ (x, y))  | cells !! ((x*10)+y) == Bomb = True
+                                            | otherwise = False
+
+    printBoard :: Board -> IO()
+    printBoard (Board (x, _) grid _) = mapM_ putStrLn (splitEvery x (map printingReplacements grid))
 
     printingReplacements:: Cell -> Char
     printingReplacements Empty = '-'
     printingReplacements Bomb = 'x'
+    printingReplacements FlagBomb = 'f'
+    printingReplacements (Neighbours 0) = ' '
+    printingReplacements (Neighbours num) = chr num
+
+    printingLostGame:: Cell -> Char
+    printingLostGame Empty = '-'
+    printingLostGame Bomb = 'x'
+    printingLostGame FlagBomb = '-'
+    printingLostGame (Neighbours num) = '-'
